4fd8bda0db52e644f2abd00eca826447f6629f30
Rahmiatul <rahmiatulhusna1109@gmail.com>
2025-06-30 13:28:17 +0700

    feat: tambah model dengan relasi dan nama tabel rahmi_ sesuai aturan UAS

--- app/Models/Models/Anggota.php
+++ app/Models/Models/Anggota.php
@@ -1,10 +1,23 @@
 <?php
 
-namespace App\Models\Models;
+namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Anggota extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'rahmi_anggotas';
+
+    protected $fillable = [
+        'nama', 'email', 'alamat',
+    ];
+
+    // Relasi ke peminjaman
+    public function peminjamans()
+    {
+        return $this->hasMany(Peminjaman::class);
+    }
 }
--- app/Models/Models/Buku.php
+++ app/Models/Models/Buku.php
@@ -1,10 +1,27 @@
 <?php
 
-namespace App\Models\Models;
+namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Buku extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'rahmi_bukus';
+
+    protected $fillable = [
+        'judul', 'penulis', 'tahun_terbit', 'kategori_id',
+    ];
+
+    public function kategori()
+    {
+        return $this->belongsTo(Kategori::class);
+    }
+
+    public function peminjamans()
+    {
+        return $this->hasMany(Peminjaman::class);
+    }
 }
